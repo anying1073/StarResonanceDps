266afd09d50eb50d685804d74c528550cd1c01ee
ihonliu <ihonliu@gmail.com>
2025-09-15 23:38:50 +0800

    feat: 添加AntDesign 开关控件，移除部分控件

---
+++ StarResonanceDpsAnalysis.WPF/Controls/SwitchControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace StarResonanceDpsAnalysis.WPF.Controls;
+
+public class SwitchControl : ToggleButton
+{
+    static SwitchControl()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(SwitchControl),
+            new FrameworkPropertyMetadata(typeof(SwitchControl)));
+    }
+
+    public static readonly DependencyProperty OnContentProperty = DependencyProperty.Register(
+        nameof(OnContent), typeof(object), typeof(SwitchControl), new PropertyMetadata("On"));
+
+    public object OnContent
+    {
+        get => GetValue(OnContentProperty);
+        set => SetValue(OnContentProperty, value);
+    }
+
+    public static readonly DependencyProperty OffContentProperty = DependencyProperty.Register(
+        nameof(OffContent), typeof(object), typeof(SwitchControl), new PropertyMetadata("Off"));
+
+    public object OffContent
+    {
+        get => GetValue(OffContentProperty);
+        set => SetValue(OffContentProperty, value);
+    }
+}
