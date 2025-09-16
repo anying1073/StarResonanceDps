dc1b64c66bdd673e991faf5b73a820893dea49ac
ihonliu <ihonliu@gmail.com>
2025-09-16 17:16:16 +0800

    refactor: 调整项目结构

--- StarResonanceDpsAnalysis.WPF/App.xaml.cs
+++ StarResonanceDpsAnalysis.WPF/App.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SharpPcap;
+using StarResonanceDpsAnalysis.Core.Data;
 using StarResonanceDpsAnalysis.WPF.Config;
 using StarResonanceDpsAnalysis.WPF.Data;
 using StarResonanceDpsAnalysis.WPF.Extensions;
